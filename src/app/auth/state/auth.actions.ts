683911a6ebc9608b9320d141d65bf83c15505e01
clintonjosephs <clintonjosephs26@gmail.com>
2023-10-19 10:06:02 +0100

    Save the User token data from Login in Ngrx State in Angular Ngrx Application

--- src/app/auth/state/auth.actions.ts
+++ src/app/auth/state/auth.actions.ts
@@ -1,4 +1,5 @@
 import { createAction, props } from "@ngrx/store";
+import { User } from "src/app/models/user.module";
 
 
 export const LOGIN_START = '[auth page] login start';
@@ -7,4 +8,4 @@ export const LOGIN_FAIL = '[auth page] login fail';
 
 export const loginStart = createAction(LOGIN_START, props<{email: string, password: string}>())
 
-export const loginSuccess = createAction(LOGIN_SUCCESS)
+export const loginSuccess = createAction(LOGIN_SUCCESS, props<{user: User}>())
