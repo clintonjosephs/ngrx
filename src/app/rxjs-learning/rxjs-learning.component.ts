803020b6523a72c60cb8acf9b580163ae578c27a
clintonjosephs <clintonjosephs26@gmail.com>
2023-12-05 08:04:37 +0100

    Interval Operator Tutorial

--- src/app/rxjs-learning/rxjs-learning.component.ts
+++ src/app/rxjs-learning/rxjs-learning.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Observable, from, fromEvent, interval, of } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs-learning',
@@ -7,29 +7,71 @@ import { Observable } from 'rxjs';
   styleUrls: ['./rxjs-learning.component.css'],
 })
 export class RxjsLearningComponent implements OnInit {
+  @ViewChild('validate', { static: true })
+  validate!: ElementRef;
+
   agents!: Observable<string>;
   agentName!: string;
 
+  students: Observable<string[]> = of(['Mark', 'Ram', 'Sita', 'Lisa']);
+
+  studentNames: Observable<string> = of('Ram');
+
+  student$: Observable<any> = of({
+    id: 10,
+    name: 'Ram'
+  })
+
+  orders$: Observable<string> = from(['Fashion', 'Electronics', 'Mobile', 'Household'])
+
   ngOnInit(): void {
-    this.agents = new Observable((observer) => {
-      try {
-        observer.next('Ram');
 
-        setInterval(() => {
-          observer.next('Mark');
-        }, 3000);
+    this.orders$.subscribe(data => {
+      const seqNum$ = interval(500);
 
-        setInterval(() => {
-          observer.next('Sita');
-        }, 6000)
-      } catch (e) {
-        observer.error(e);
-      }
+      seqNum$.subscribe(num => {
+        if (num < 5) {
+          console.log(data + num);
+        }
+      });
+
+      console.log(data);
     });
 
-    this.agents.subscribe(data => {
+    // this.students.subscribe((data => console.log(data)));
+
+    // this.orders$.subscribe((data => console.log(data)));
+
+    // this.agents = new Observable((observer) => {
+    //   try {
+    //     observer.next('Ram');
+
+    //     setInterval(() => {
+    //       observer.next('Mark');
+    //     }, 3000);
+
+    //     setInterval(() => {
+    //       observer.next('Sita');
+    //     }, 6000)
+    //   } catch (e) {
+    //     observer.error(e);
+    //   }
+    // });
+
+    // this.agents.subscribe(data => {
+    //   console.log(data);
+    //   this.agentName = data;
+    // })
+
+
+
+
+  }
+
+  rxjsEventObservable() {
+    const btnObservable$ = fromEvent(this.validate?.nativeElement, 'click');
+    btnObservable$.subscribe((data) => {
       console.log(data);
-      this.agentName = data;
     })
   }
 }
